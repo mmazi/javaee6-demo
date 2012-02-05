fef4397fc4919537566fb745ce3806cc57ddaec3
mmazi <matija.mazi@gmail.com>
2012-02-05 18:42:24 +0100

    Basic data, basic queries

---
+++ web/src/main/java/com/housing/javaee6demo/PersistentDataInitializer.java
@@ -0,0 +1,78 @@
+package com.housing.javaee6demo;
+
+import com.housing.javaee6demo.model.Order;
+import com.housing.javaee6demo.model.OrderItem;
+import com.housing.javaee6demo.model.Product;
+import com.housing.javaee6demo.model.Role;
+import com.housing.javaee6demo.model.Unit;
+import com.housing.javaee6demo.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Arrays;
+
+/**
+ * @author Matija Mazi <br/>
+ * @created 5.2.12 13:49
+ */
+@Startup @Singleton
+public class PersistentDataInitializer {
+    private static final Logger log = LoggerFactory.getLogger(PersistentDataInitializer.class);
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @PostConstruct
+    public void initData() {
+        User mm = new User("mm", "a", "Matija", Arrays.asList(Role.admin, Role.orderer));
+        Order order = new Order(mm);
+
+        Unit no = new Unit("št.", "");
+        Unit kg = new Unit("kg", "Kilogram");
+
+        Product glasses = new Product("Očala", no, null);
+        Product ladder = new Product("Lestev", no, null);
+        Product flour = new Product("Moka", kg, null);
+
+        order.addItem(new OrderItem(order, glasses, 2.));
+        order.addItem(new OrderItem(order, flour, 1.3));
+        order.addItem(new OrderItem(order, ladder, 3.));
+
+        em.persist(mm);
+        em.persist(no);
+        em.persist(kg);
+        em.persist(glasses);
+        em.persist(flour);
+        em.persist(ladder);
+        em.persist(order);
+
+        em.flush();
+
+        log.debug("Done initializing, now querying...");
+
+        log.debug("kg unit (find): {}", em.find(Unit.class, "kg"));
+        log.debug("kg unit (query): {}", em.createQuery("select u from Unit u where u.id = :uid", Unit.class).setParameter("uid", "kg").getSingleResult());
+        log.debug("Moka: {}",
+                em.createQuery("select p from Product p where p.id = :pid", Product.class)
+                .setParameter("pid", flour.getId())
+                .getSingleResult()
+        );
+
+        Number totalItems = em.createQuery(
+                "select sum(i.quantity) " +
+                        "from Order o join o.items i join i.product p " +
+                        "where p.unit.id = :nounitid and o.creator.username = :cun", Number.class)
+                .setParameter("nounitid", "št.")
+                .setParameter("cun", "mm")
+                .getSingleResult();
+
+        log.debug("totalItems = {}", totalItems);
+
+    }
+
+}

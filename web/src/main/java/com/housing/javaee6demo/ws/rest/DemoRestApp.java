f898867fa1cf0ffbb84e356ad6a4db51720e66c9
mmazi <matija.mazi@gmail.com>
2012-02-12 09:55:39 +0100

    jax-rs config

---
+++ web/src/main/java/com/housing/javaee6demo/ws/rest/DemoRestApp.java
@@ -0,0 +1,7 @@
+package com.housing.javaee6demo.ws.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class DemoRestApp extends Application { }
